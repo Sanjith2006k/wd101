3e13efe7652ffff1fe092b35d9647b8f525e4c61
S Sanjith Kumar <ssanjithkumar2006@gmail.com>
2025-05-11 16:05:03 +0530

    index.js

--- index.js
+++ index.js
@@ -56,8 +56,3 @@ const saveusform = (event) => {
 
 uf.addEventListener("submit", saveusform);
 displayEntries();
-const today = new Date();
-const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
-const minDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate());
-document.getElementById("dob").max = maxDate.toISOString().split("T")[0];
-document.getElementById("dob").min = minDate.toISOString().split("T")[0];
